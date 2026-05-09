f3f69b4d98399698e9b50b04f41f8f804464e146
Saurav377 <mssaurav377@gmail.com>
2026-05-09 15:52:39 +0530

    added supabase

--- public/supabase-client.js
+++ public/supabase-client.js
@@ -1,6 +1,6 @@
 // public/supabase-client.js
-const supabaseUrl = 'https://ynvykdraupxkhsxxsonb.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InludnlrZHJhdXB4a2hzeHhzb25iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzMTIyMDUsImV4cCI6MjA4ODg4ODIwNX0.DRdCi6jxts3i9g0vTaRevRcIB4xfEadqxxX_d3DYzvA';
+const supabaseUrl = 'https://qzfimxpunguvhzljjkyr.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF6ZmlteHB1bmd1dmh6bGpqa3lyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyODIxODYsImV4cCI6MjA5Mzg1ODE4Nn0.HYDSlhAC2EexuIZQW-92VzHD_zYFk6Ruufn5NXlhKg4';
 window.supabaseClient = window.supabase.createClient(supabaseUrl, supabaseKey);
 
 const defaultStorageBuckets = {
